08dda2e5202209860c4e08d1f78e034ec56723e9
Flame1611 <113800369+Flame1611@users.noreply.github.com>
2023-09-06 13:14:16 +0100

    Added name cards, added proper ace funtionality
    Changed 3 of the 10s for each suit to a K, Q and J, and the ones to A, for King, Queen, Jack and Ace respectively, and added the rule where aces are worth 1 or 11 to value calculations.

--- blackjack.py
+++ blackjack.py
@@ -7,9 +7,24 @@ def add_card(deck,hand,ammount):
         deck.remove(deck[index])
 
 def hand_val(hand):
+    special_cards = {
+        "A" : "11",
+        "J" : "10",
+        "Q" : "10",
+        "K" : "10"
+    }
+    aces = 0
     value = 0
     for i in hand:
-        value += int(i[:len(i)-1])
+        try:
+            value += int(i[:len(i)-1])
+        except Value Error:
+            if i[:len(i)-1] == "A":
+                aces += 1
+            value += special_cards[i[:len(i)-1]]
+    while value > 21 and aces > 0:
+        value -= 10
+        aces -= 1
     return(value)
 
 def board(dealer,player):
@@ -29,10 +44,10 @@ def dealer_ai(hand):
         return(hand)
 option = "y"
 while option == "y":
-    deck = ["1s","2s","3s","4s","5s","6s","7s","8s","9s","10s","10s","10s","10s"
-            ,"1c","2c","3c","4c","5c","6c","7c","8c","9c","10c","10c","10c","10c"
-            ,"1h","2h","3h","4h","5h","6h","7h","8h","9h","10h","10h","10h","10h"
-            ,"1d","2d","3d","4d","5d","6d","7d","8d","9d","10d","10d","10d","10d"]
+    deck = ["As","2s","3s","4s","5s","6s","7s","8s","9s","10s","Js","Qs","Ks"
+            ,"Ac","2c","3c","4c","5c","6c","7c","8c","9c","10c","Jc","Qc","Kc"
+            ,"Ah","2h","3h","4h","5h","6h","7h","8h","9h","10h","Jh","Qh","Kh"
+            ,"Ad","2d","3d","4d","5d","6d","7d","8d","9d","10d","Jd","Qd","Kd"]
     dealer = []
     player = []
 
